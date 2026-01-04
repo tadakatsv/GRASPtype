3e2654bcfb85924c954fa746c5e7481ea9145adb
izanami <dargberimor@gmail.com>
2026-01-04 18:48:03 +0200

    Adding getters and setters and more Main demonstration

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,6 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User();
 
         Address address = new Address(
                 "Odessa",
@@ -10,8 +9,13 @@ public class Main {
                 "3"
         );
 
+        User user = new User("Mikhail", "Tchekmaryov", new Address("Kiev","Kolotushkina","12/3"));
+
+        System.out.println("User before setting new address: " + user);
+
         user.setAddress(address);
 
+        System.out.println("User after setting new address: " + user);
         System.out.println("User address: " + user.getAddress());
     }
 }
--- src/app/User.java
+++ src/app/User.java
@@ -5,11 +5,40 @@ public class User {
     private String surname;
     private Address address;
 
+    public User(String name, String surname, Address address) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
     public Address getAddress() {
         return address;
     }
 
     public void setAddress(Address address) {
         this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address=" + address;
     }
 }
